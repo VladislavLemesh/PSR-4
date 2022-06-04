b330d3a24660e6846672abd8889a9d3181cad194
VladislavLemesh <90740323+VladislavLemesh@users.noreply.github.com>
2022-06-04 11:25:55 +1000

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -1,9 +1,5 @@
 <?php
-spl_autoload_register(function ($className) {
-  $path = 'src/';
-  $fullPath = dirname(__DIR__ ) . '/' . $path . str_replace("\\", "/", $className) . '.php';
-  require_once $fullPath;
-});
+require_once __DIR__ . '/../autoload.php';
 
 use nestedclass\Class2;
 use nestedclass\nestedinnestedclass\Class3;
@@ -14,4 +10,3 @@ $class3 = new Class3();
 $class1->use_class1();
 $class2->use_class2();
 $class3->use_class3();
-?>
